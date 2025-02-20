b570ea360210de8c403956067cdd2228c4c32610
David <davidfco.pozo@gmail.com>
2025-02-20 19:53:53 +0100

    Add reCAPTCHA and Google account configuration to example config
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- config.example.php
+++ config.example.php
@@ -1,3 +1,7 @@
 <?php
 const GOOGLE_SHEETS_API_KEY = 'Your Google Sheets API key';
 const SPREADSHEET_ID = 'The given spread sheet id';
+const RECAPTCHA_PUBLIC = 'Your recaptcha public key';
+const RECAPTCHA_SECRET = 'Your recaptcha secret key';
+const GOOGLE_EMAIL = 'Your Google account';
+const GOOGLE_APP_PWD = 'Your Google account app password';
