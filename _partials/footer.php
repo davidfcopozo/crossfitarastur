b798e7ae17f2003f83dbcc7720df04b5e717e595
David <davidfco.pozo@gmail.com>
2025-02-25 18:53:32 +0100

    Update footer logo link for improved accessibility and remove unused scroll-to-top feature
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- _partials/footer.php
+++ _partials/footer.php
@@ -4,7 +4,7 @@
             <div class="col-lg-3">
                 <div class="footer-logo-item">
                     <div class="f-logo">
-                        <a href="#"><img src="../images/logo.webp" alt="Logo de CrossFit Arastur"></a>
+                        <a href="/" title="Inicio"><img src="../images/logo.webp" alt="Logo de CrossFit Arastur" loading="lazy"></a>
                     </div>
                     <p>
                         CrossFit Arastur es la sede de Built By Odin, marca de
@@ -87,24 +87,7 @@
 <a id="whatsapp-icon" class="whatsapp-icon" href="https://wa.me/+34649238717" target="_blank">
     <i class="fa-brands fa-whatsapp fa-lg" style="color: #ffffff;"></i>
 </a>
-<!-- Scroll To Top -->
-<a id="back-top" class="back-to-top page-scroll" href="#main">
-    <svg
-        version="1.1"
-        id="Layer_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        viewBox="0 0 122.88 66.91"
-        style="enable-background: new 0 0 122.88 66.91"
-        xml:space="preserve">
-        <g>
-            <path
-                d="M11.68,64.96c-2.72,2.65-7.08,2.59-9.73-0.14c-2.65-2.72-2.59-7.08,0.13-9.73L56.87,1.97l4.8,4.93l-4.81-4.95 c2.74-2.65,7.1-2.58,9.76,0.15c0.08,0.08,0.15,0.16,0.23,0.24L120.8,55.1c2.72,2.65,2.78,7.01,0.13,9.73 c-2.65,2.72-7,2.78-9.73,0.14L61.65,16.5L11.68,64.96L11.68,64.96z" />
-        </g>
-    </svg>
-</a>
+
 <script>
     const RECAPTCHA_PUBLIC = '<?php echo RECAPTCHA_PUBLIC; ?>';
 </script>
