dde663356f04774a20ab94126971f4aecedad415
David <davidfco.pozo@gmail.com>
2025-02-25 18:52:45 +0100

    Refactor FAQ section to use Bootstrap accordion for improved user experience and styling
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- _partials/faq.php
+++ _partials/faq.php
@@ -1,40 +1,56 @@
-<section class="faq-accordion" role="region" aria-label="Preguntas frecuentes">
+<section class=" faq-accordion accordion accordion accordion-flush" id="faqAccordion" role="region" aria-label="Preguntas frecuentes">
     <div class="container">
         <h2>Preguntas frecuentes</h2>
 
-        <details class="faq-item">
-            <summary class="faq-question">
-                ¿Necesito estar en forma para empezar CrossFit?
-            </summary>
-            <div class="faq-answer">
-                <p>No es necesario que vengas con una condición física previa, nosotros te enseñaremos las bases del deporte y te ayudaremos a mejorar paso a paso. No importa si has practicado previamente otras disciplinas deportivas o no, simplemente trae al box ganas y motivación y te adaptaremos los wod todo lo que necesites.</p>
+        <div class="accordion-item faq-item ">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    ¿Necesito estar en forma para empezar CrossFit?
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#faqAccordion">
+                <div class="accordion-body">
+                    <p>No es necesario que vengas con una condición física previa, nosotros te enseñaremos las bases del deporte y te ayudaremos a mejorar paso a paso. No importa si has practicado previamente otras disciplinas deportivas o no, simplemente trae al box ganas y motivación y te adaptaremos los wod todo lo que necesites.</p>
+                </div>
             </div>
-        </details>
+        </div>
+        <div class="faq-item accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    Y si tengo problemas de salud, ¿puedo practicar CrossFit?
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#faqAccordion">
+                <div class="accordion-body">
+                    <p>Hernias discales, problemas de menisco, cartílago o ligamentos de la rodilla, tendinosis de larga evolución… En nuestro servicio de readaptación deportiva te haremos una entrevista y valoración inicial. Somos el único centro en Aragón que cuenta en la plantilla con una médica del deporte y especialista en CrossFit desde que llegó esta disciplina a España en el año 2011. Podemos adaptar los ejercicios para mejorar tu funcionalidad y tu calidad de vida, sin perjudicar tu patología.</p>
+                </div>
+            </div>
+        </div>
+        <div class="faq-item accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                    ¿Puede una persona con movilidad reducida hacer CrossFit?
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#faqAccordion">
+                <div class="accordion-body">
+                    <p>Nuestro centro cumple con toda la normativa vigente para personas con movilidad reducida. CrossFit se puede adaptar a cualquier condición física, así que, si estás interesado en probar una nueva disciplina deportiva, en CrossFit Arastur no encontrarás barreras para adentrarte en nuestras sesiones de entrenamiento diarias.</p>
+                </div>
+            </div>
+        </div>
+        <div class="faq-item accordion-item">
+            <h2 class="accordion-header" id="headingFour">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                    ¿Qué niveles de entrenamiento hay y en qué se diferencian?
+                </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#faqAccordion">
+                <div class="accordion-body">
+                    <p>Encontramos fundamentalmente dos niveles: RX y escalado. En el nivel RX debemos de ejecutar el entrenamiento tal y como se describe en cuanto a pesos, repeticiones y dificultad de los movimientos. Para llegar a este nivel se requiere un tiempo de aprendizaje y de progresión física. El nivel escalado es aquel en el que estamos cuando no llegamos a cumplir la exigencia de lo que se pide en el nivel RX. Cuando trabajamos en escalado pondremos un peso y unos movimientos acordes a nuestro nivel.</p>
+                </div>
+            </div>
+        </div>
 
-        <details class="faq-item">
-            <summary class="faq-question">
-                Y si tengo problemas de salud, ¿puedo practicar CrossFit?
-            </summary>
-            <div class="faq-answer">
-                <p>Hernias discales, problemas de menisco, cartílago o ligamentos de la rodilla, tendinosis de larga evolución… En nuestro servicio de readaptación deportiva te haremos una entrevista y valoración inicial. Somos el único centro en Aragón que cuenta en la plantilla con una médica del deporte y especialista en CrossFit desde que llegó esta disciplina a España en el año 2011. Podemos adaptar los ejercicios para mejorar tu funcionalidad y tu calidad de vida, sin perjudicar tu patología.</p>
-            </div>
-        </details>
 
-        <details class="faq-item">
-            <summary class="faq-question">
-                ¿Puede una persona con movilidad reducida hacer CrossFit?
-            </summary>
-            <div class="faq-answer">
-                <p>Nuestro centro cumple con toda la normativa vigente para personas con movilidad reducida. CrossFit se puede adaptar a cualquier condición física, así que, si estás interesado en probar una nueva disciplina deportiva, en CrossFit Arastur no encontrarás barreras para adentrarte en nuestras sesiones de entrenamiento diarias.</p>
-            </div>
-        </details>
-        <details class="faq-item">
-            <summary class="faq-question">
-                ¿Qué niveles de entrenamiento hay y en qué se diferencian?
-            </summary>
-            <div class="faq-answer">
-                <p>Encontramos fundamentalmente dos niveles: RX y escalado. En el nivel RX debemos de ejecutar el entrenamiento tal y como se describe en cuanto a pesos, repeticiones y dificultad de los movimientos. Para llegar a este nivel se requiere un tiempo de aprendizaje y de progresión física. El nivel escalado es aquel en el que estamos cuando no llegamos a cumplir la exigencia de lo que se pide en el nivel RX. Cuando trabajamos en escalado pondremos un peso y unos movimientos acordes a nuestro nivel.</p>
-            </div>
-        </details>
     </div>
 </section>
